21501fa453526a09adaea0cf935dc34ef875e59a
clr1235 <15035291867@163.com>
2024-03-27 10:31:25 +0800

    chore(root): 配置eslint

---
+++ eslint.config.ts
@@ -0,0 +1,11 @@
+// eslint.config.mjs
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+    // enable UnoCSS support
+    // https://unocss.dev/integrations/vscode
+    unocss: true,
+    ignores: [
+        '.github',
+    ],
+})
