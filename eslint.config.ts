afa4cfcfc61d30a285c34f8bf622713a44dd9511
clr1235 <15035291867@163.com>
2024-03-27 15:20:49 +0800

    chore(root): eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,14 +2,16 @@
 import antfu from '@antfu/eslint-config'
 
 
-const data = antfu({
+const conf = antfu({
     // enable UnoCSS support
     // https://unocss.dev/integrations/vscode
     unocss: true,
+    typescript: true,
+    vue: true,
     ignores: [
         '.github',
     ],
 })
-console.log(data, 'data=s=s=s=s=')
+console.log(conf, 'data=s=s=s=s=')
 
-export default data
+export default conf
