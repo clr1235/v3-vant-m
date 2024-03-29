d87932b3e59e6592ec721f598130d716671b6b22
clr1235 <15035291867@163.com>
2024-03-29 10:35:26 +0800

    feat(root): 引入vant

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { ConfigProvider } from 'vant';
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import './style.css';
+
+/* --------------------------------
+Vant 中有个别组件是以函数的形式提供的，
+包括 Toast，Dialog，Notify 和 ImagePreview 组件。
+在使用函数组件时，unplugin-vue-components
+无法自动引入对应的样式，因此需要手动引入样式。
+------------------------------------- */
+import 'vant/es/dialog/style';
+import 'vant/es/image-preview/style';
+import 'vant/es/notify/style';
+import 'vant/es/toast/style';
+
+const app = createApp(App);
+
+app.use(ConfigProvider)
+app.mount('#app')
