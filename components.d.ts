568a78d2e85c59f3b2c3d3070fb933b0f57419d2
clr1235 <15035291867@163.com>
2024-04-01 14:45:16 +0800

    feat(root): 添加底部tab

--- components.d.ts
+++ components.d.ts
@@ -10,5 +10,7 @@ declare module 'vue' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     VanConfigProvider: typeof import('vant/es')['ConfigProvider']
+    VanTabbar: typeof import('vant/es')['Tabbar']
+    VanTabbarItem: typeof import('vant/es')['TabbarItem']
   }
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>默认页面</div>
+    <van-tabbar v-model="active">
+        <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+        <van-tabbar-item icon="search">标签</van-tabbar-item>
+        <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+        <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
 </template>
 
 <script setup lang="ts">
-
+import { ref } from 'vue';
+const active = ref(0);
 </script>
 
 <style lang="less" scoped></style>
